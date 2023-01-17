25c3cd8a4160f6810adbe041f9f7ba7d0e1ab8ce
catfo <pranavlsgreat210120@gmail.com>
2023-01-17 22:34:30 +0530

    Added new cat button

--- src/CatPic.jsx
+++ src/CatPic.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 export default class CatPic extends Component {
   constructor(props) {
     super(props)
@@ -9,11 +10,20 @@ export default class CatPic extends Component {
       breedName: 'Fetching a cat for you...'
     }
 
-    this.getData();
+    this.getCatData();
 
   }
 
-  getData() {
+  handleClick = () => {
+    this.setState({
+      imageUrl: 'https://media.tenor.com/RVvnVPK-6dcAAAAM/reload-cat.gif',
+      breedName: 'Fetching a cat for you...'
+    }
+    )
+    this.getCatData()
+  }
+
+  getCatData() {
     const apiURL = 'https://api.thecatapi.com/v1/images/search?has_breeds=1'
     const apiKEY = '63b9a514-aad1-40fd-bb8c-d50b5ed28db3'
 
@@ -25,7 +35,7 @@ export default class CatPic extends Component {
       .then((response) => { return response.json() })
       .then((data) => {
         if (data[0]['height'] !== data[0]['width']) {
-          this.getData()
+          this.getCatData()
           return
         }
         this.setState({
@@ -34,15 +44,23 @@ export default class CatPic extends Component {
         }
         )
       })
-      .catch(console.error) //implement error later
+      .catch(() => {
+        alert('Timeout, try again after a few seconds!')
+        this.setState({
+          imageUrl: "https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/2/2022/05/cat-yawning-other-cats.jpg",
+          breedName: "Try after a few seconds"
+        })
+      }) //implement error later
   }
 
   render() {
     return (
       <>
         <img src={this.state.imageUrl} alt={`A ${this.state.breedName}`} className="cat" />
+        <button className='next-cat-button' onClick={this.handleClick}>Get a new cat uwu</button>
         <h2 className='breedName'>{this.state.breedName}</h2>
       </>
     )
   }
 }
+//have to implement gradient change when new cat appears
